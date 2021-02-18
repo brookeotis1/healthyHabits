7a126075d247180ba5cc5419423123a6d956b698
Juan Boza <Juanboza80@gmail.com>
2021-02-17 20:08:35 -0700

    components added to source folder

---
+++ healthy-habits/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import './App.css';
+import Header from './components/header'
+//import Main from './components/main'
+//import Footer from './components/footer' 
+import Button from './components/buttons'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main className="main">
+        <Button /> 
+      
+      </main> 
+      
+    </div>
+   
+  );
+}
+
+export default App;
---
+++ healthy-habits/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App';
+
+
+
+ReactDOM.render((
+  <BrowserRouter>
+    <App /> {/* The various pages will be displayed by the `Main` component. */}
+  </BrowserRouter>
+  ), document.getElementById('root')
+);
+
+
