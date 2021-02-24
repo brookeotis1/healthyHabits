fd772da1c597439a3bed983ef4bbb90c951ef9c6
Brianne <brianne.ewell1@gmail.com>
2021-02-24 15:36:49 -0700

    adding card to profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,22 @@
-import React, { Component } from "react";
+import React, { Component, useState, Link } from "react";
+import DisplayGoal from "../components/Card/GoalCard";
+
+function PullGoals(){
+    const [savedGoals, setSavedGoals] = useState([])
+}
 
 class Profile extends Component {
     render() {
         return(
             <div>
                 <h1>Profile Page</h1>
-            </div>
+            
+       <DisplayGoal
+       header={"test header"}
+       title={"test title"}
+       text={savedGoals}
+       />
+        </div> 
         )
     }
 }
