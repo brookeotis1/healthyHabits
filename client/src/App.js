efd1d21969c86eb71b1421ed27e10630ddd0b329
Brianne <brianne.ewell1@gmail.com>
2021-02-24 20:46:38 -0700

    backend connected

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,9 @@
 import React, { Component } from "react";
-import './App.css';
 import Header from './components/header/navbar'
-//import Main from './components/main'
-//import Footer from './components/footer' 
-//import Button from './components/buttons'
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-//import NavTabs from "./pages/NavTabs";
-//import Goal from "./pages/newGoal";
 import Main from "./components/main";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import DisplayGoal from "./components/Card/GoalCard.js"
+import './app.css';
 
-
-//import Signup from "./pages/Signup";
-
-
-
-//state that houses form details (pull from state to render on page)
 
 class App extends Component {
   
@@ -39,18 +26,3 @@ export default App;
 
 
 
-    /*<Router>
-      <div>
-
-
-        <Route exact path="/signup" component={Signup} />
-
-      </div>
-    </Router>
-    
-  );
-}
-
-export default App; 
-
-*/
