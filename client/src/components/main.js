b18284d4c8c4fbe6d0a400a9e574c44312567476
Brianne <brianne.ewell1@gmail.com>
2021-03-01 21:40:45 -0700

    fixed routes

--- client/src/components/main.js
+++ client/src/components/main.js
@@ -1,38 +1,38 @@
-import React from 'react';
-import { Profile } from '../pages/Profile';
-import { Switch, Route} from 'react-router-dom';
-import Goals from "../pages/Goals";
-import Checkin from "../pages/Checkin";
-import Progress from "../pages/Progress";
-import Notes from "../pages/Notes";
-import Resources from "../pages/Resources"
-import Stress from "../pages/resourcePages/stress"
-import Smart from "../pages/resourcePages/SMART"
-// import NoMatch from "../pages/NoMatch";
-import { Login } from "../pages/Login";
-import { Register } from "../pages/Register";
+// import React from 'react';
+// import { Profile } from '../pages/Profile';
+// import { Switch, Route} from 'react-router-dom';
+// import Goals from "../pages/Goals";
+// import Checkin from "../pages/Checkin";
+// import Progress from "../pages/Progress";
+// import Notes from "../pages/Notes";
+// import Resources from "../pages/Resources"
+// import Stress from "../pages/resourcePages/stress"
+// import Smart from "../pages/resourcePages/SMART"
+// // import NoMatch from "../pages/NoMatch";
+// import Login  from "../pages/login";
+// import Register from "../pages/register";
 
-const Main = () => {
+// const Main = () => {
 
-    return(
-        <Switch>
-            <Route path="/profile" component= {Profile}></Route>
-            <Route path="/goals" component= {Goals}></Route>
-            <Route path="/checkin" component= {Checkin}></Route>
-            <Route path="/progress" component= {Progress}></Route>
-            <Route path="/goals/:id" component= {Notes}></Route>
+//     return(
+//         <Switch>
+//             <Route path="/profile" component= {Profile}></Route>
+//             <Route path="/goals" component= {Goals}></Route>
+//             <Route path="/checkin" component= {Checkin}></Route>
+//             <Route path="/progress" component= {Progress}></Route>
+//             <Route path="/goals/:id" component= {Notes}></Route>
 
-            <Route path="/resources" component= {Resources}></Route>
-            <Route path="/stress" component= {Stress}></Route>
-            <Route path="/smart" component= {Smart}></Route>
+//             <Route path="/resources" component= {Resources}></Route>
+//             <Route path="/stress" component= {Stress}></Route>
+//             <Route path="/smart" component= {Smart}></Route>
 
-            <Route path="/login" component= {Login}></Route>
-            <Route path="/register" component= {Register}></Route>
+//             <Route path="/login" component= {Login}></Route>
+//             <Route path="/register" component= {Register}></Route>
 
-            {/* <Route component= {NoMatch}></Route> */}
-        </Switch>
+//             {/* <Route component= {NoMatch}></Route> */}
+//         </Switch>
 
-    )
-}
+//     )
+// }
 
-export default Main;
+// export default Main;
