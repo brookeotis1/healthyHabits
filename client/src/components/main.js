025185284849ffcb1a8760cf0d15411e94c0129b
Brianne <brianne.ewell1@gmail.com>
2021-03-01 17:10:46 -0700

    adding resources section

--- client/src/components/main.js
+++ client/src/components/main.js
@@ -5,6 +5,8 @@ import Goals from "../pages/Goals";
 import Checkin from "../pages/Checkin";
 import Progress from "../pages/Progress";
 import Notes from "../pages/Notes";
+import Resources from "../pages/Resources"
+import Stress from "../pages/resourcePages/stress"
 // import NoMatch from "../pages/NoMatch";
 
 const Main = () => {
@@ -16,6 +18,8 @@ const Main = () => {
             <Route path="/checkin" component= {Checkin}></Route>
             <Route path="/progress" component= {Progress}></Route>
             <Route path="/goals/:id" component= {Notes}></Route>
+            <Route path="/resources" component= {Resources}></Route>
+            <Route path="/stress" component= {Stress}></Route>
             {/* <Route component= {NoMatch}></Route> */}
         </Switch>
 
