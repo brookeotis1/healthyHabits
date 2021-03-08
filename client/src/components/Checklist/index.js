b3d06a0bf40cd5d4bcfc1cba89808e11144f7c32
Brooke Maneotis <xmaneotisb@yahoo.com>
2021-03-08 13:51:11 -0700

    add to Check-in page

---
+++ client/src/components/Checklist/index.js
@@ -0,0 +1,75 @@
+import React from "react";
+
+    function Checklist() {
+      return (
+        <div>
+            <h3>Check in: Practice piano 20 min a day</h3>
+          <div className="form-check form-check-inline">
+            <input className="form-check-input" type="checkbox" id="inlineCheckbox1" defaultValue="option1" />
+            <label className="form-check-label" htmlFor="inlineCheckbox1">Monday</label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input className="form-check-input" type="checkbox" id="inlineCheckbox2" defaultValue="option2" />
+            <label className="form-check-label" htmlFor="inlineCheckbox2">Tuesday</label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input className="form-check-input" type="checkbox" id="inlineCheckbox3" defaultValue="option3" />
+            <label className="form-check-label" htmlFor="inlineCheckbox3">Wednesday</label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input className="form-check-input" type="checkbox" id="inlineCheckbox2" defaultValue="option4" />
+            <label className="form-check-label" htmlFor="inlineCheckbox2">Thursday</label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input className="form-check-input" type="checkbox" id="inlineCheckbox2" defaultValue="option5" />
+            <label className="form-check-label" htmlFor="inlineCheckbox2">Friday</label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input className="form-check-input" type="checkbox" id="inlineCheckbox2" defaultValue="option6" />
+            <label className="form-check-label" htmlFor="inlineCheckbox2">Saturday</label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input className="form-check-input" type="checkbox" id="inlineCheckbox2" defaultValue="option7" />
+            <label className="form-check-label" htmlFor="inlineCheckbox2">Sunday</label>
+          </div>
+          <button>Save</button>
+          
+          <br />
+
+          <h3>Check in: Eat 3 servings of veggies</h3>
+          <div className="form-check form-check-inline">
+            <input className="form-check-input" type="checkbox" id="inlineCheckbox1" defaultValue="option1" />
+            <label className="form-check-label" htmlFor="inlineCheckbox1">Monday</label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input className="form-check-input" type="checkbox" id="inlineCheckbox2" defaultValue="option2" />
+            <label className="form-check-label" htmlFor="inlineCheckbox2">Tuesday</label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input className="form-check-input" type="checkbox" id="inlineCheckbox3" defaultValue="option3" />
+            <label className="form-check-label" htmlFor="inlineCheckbox3">Wednesday</label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input className="form-check-input" type="checkbox" id="inlineCheckbox2" defaultValue="option4" />
+            <label className="form-check-label" htmlFor="inlineCheckbox2">Thursday</label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input className="form-check-input" type="checkbox" id="inlineCheckbox2" defaultValue="option5" />
+            <label className="form-check-label" htmlFor="inlineCheckbox2">Friday</label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input className="form-check-input" type="checkbox" id="inlineCheckbox2" defaultValue="option6" />
+            <label className="form-check-label" htmlFor="inlineCheckbox2">Saturday</label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input className="form-check-input" type="checkbox" id="inlineCheckbox2" defaultValue="option7" />
+            <label className="form-check-label" htmlFor="inlineCheckbox2">Sunday</label>
+          </div>
+          <button>Save</button>
+        </div>
+      );
+    }
+  
+
+
+export default Checklist;
--- client/src/pages/Checkin.js
+++ client/src/pages/Checkin.js
@@ -1,13 +1,26 @@
 import React, { Component } from "react";
+import Checklist from "../components/Checklist";
+import { Container } from "../components/Grid";
+
 
 class Checkin extends Component {
     render() {
         return(
             <div>
-                <h1>Check-in Page</h1>
+                
+                <Container>
+                <Checklist />
+                </Container>
+                
+                
+                
+                
             </div>
         )
     }
 }
 
 export default Checkin;
+
+
+{/* <h1>Check-in Page</h1> */}
